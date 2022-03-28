90d01daf24391fcda64dc4f684cd65a593fda352
Simen Kathirgamadas <simen.kathir@gmail.com>
2022-03-28 15:22:21 +0200

    small changes

--- shell.c
+++ shell.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
-#include <error.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <errno.h>
+#include <mach/error.h>
 
 #define clear() printf("\033[H\033[J")
 #define MAX_LIMIT 1024
@@ -22,97 +22,66 @@ char input[MAX_LIMIT];
 char *parsed[MAX_ARGS];
 char seps[] = " \t\r\n";
 
+// initalizes shell
 void init_shell()
 {
     clear();
     printf("\n\n\n\n******************"
-        "************************");
-    printf("\n\n\n\t****MY SHELL****");
-    printf("\n\n\t-USE AT YOUR OWN RISK-");
+           "************************");
+    printf("\n\n\n\tWELCOME TO MATRIX");
+    printf("\n\n\tBLUE OR RED PILL?");
     printf("\n\n\n\n*******************"
-        "***********************");
-    char* username = getenv("USER");
+           "***********************");
+    char *username = getenv("USER");
     printf("\n\n\nUSER is: @%s", username);
     printf("\n");
-    sleep(1);
+    sleep(3);
     clear();
 }
 
+// prints directory path
 void printDir()
 {
     getcwd(cwd, sizeof(cwd));
     printf("Dir: %s: ", cwd);
 }
 
-void take_input() {
+// take input from user
+void take_input()
+{
     fgets(input, MAX_LIMIT, stdin);
 }
 
-void process_input() {
-
+// process input from user
+void process_input()
+{
     char *token;
     int count = 0;
-    
-    token = strtok( input, seps );
-
-    while( token != NULL )
+    token = strtok(input, seps);
+    while (token != NULL)
     {
-        /* While there are tokens in "string" */
         parsed[count++] = token;
-        /* Get next token: */
-        token = strtok( NULL, seps );
+        token = strtok(NULL, seps);
     }
     parsed[count] = NULL;
-
-    
-    // for (int i = 0; i < MAX_ARGS; i++) {     
-    //     printf("%s \n", parsed[i]);     
-    // }    
-    int status;
-    
-    if (command_handler() == 1 ) {
-        return;
-    } else {
-        //TODO
-        exec_command();
-    }
+    int status = command_handler();
 }
 
-// void command_executer() {
-
-//     // TODO: fix zombies
-//     int exit_status;
-//     pid_t pid = fork();
-
-//     if (pid < 0) {
-//         printf("Failed to create child process\n");
-//         exit(0);                
-//     } else if (pid == 0) {
-//         exit_status = command_handler();
-//         exit(0);
-//     } else {
-//         wait(0);
-//         int status;
-//         waitpid(pid, &status, 0);
-//         printf("child process finished\n");
-//         return;
-//     }
-// }
-
-int command_handler() {
+int command_handler()
+{
 
     int num_commands = 3, commandswitch = 0;
-    char* commands[num_commands];
-    
+    char *commands[num_commands];
+
     commands[0] = "exit";
     commands[1] = "cd";
     commands[2] = "help";
+    // int num_commands = sizeof(commands);
 
-    int i;
-
-    for (i = 0; i < num_commands; i++)
+    for (int i = 0; i < num_commands; i++)
     {
-        if (strcmp(parsed[0], commands[i]) == 0) {
+        if (strcmp(parsed[0], commands[i]) == 0)
+        {
             commandswitch = i + 1;
             break;
         }
@@ -121,63 +90,66 @@ int command_handler() {
     switch (commandswitch)
     {
     case 1:
-        printf("Goodbye motherfucker!\n");
+        printf("Exit's shell. Have a nice day!\n");
         exit(0);
         break;
-    
+
     case 2:
         chdir(parsed[1]);
         return 1;
 
     case 3:
         printf("List of available commands: \n");
-        for (int i = 0; i < num_commands; i++) {     
-            printf("\t%s \n", commands[i]);     
+        for (int i = 0; i < num_commands; i++)
+        {
+            printf("\t%s \n", commands[i]);
         }
         return 1;
-    
+
     default:
-        // printf("Command '%s' not found.\n", parsed[0]);
+        exec_command();
         break;
     }
-    
+
     return 0;
 }
 
-void exec_command() {
-
-    // Create child process
+// execute command
+void exec_command()
+{
     pid_t pid = fork();
 
-    if (pid < 0) {
+    if (pid < 0)
+    {
         printf("Failed to create child process\n");
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         printf("Executing %s\n", parsed[0]);
-        if (execvp(parsed[0], parsed) < 0) {
-            error(0, errno, "Could not execute command");
+        if (execvp(parsed[0], parsed) < 0)
+        {
+            unix_err(errno);
         }
         exit(0);
-    } else {
+    }
+    else
+    {
         int status;
         waitpid(pid, &status, 0);
         return;
     }
 }
-  
 
 int main(int argc, char const *argv[])
 {
     init_shell();
 
     while (1)
-    {   
+    {
         printDir();
         take_input();
-        //exec_command();
         process_input();
-        
     }
-    
 
     return 0;
 }
